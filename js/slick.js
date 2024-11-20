0971577ca2b81882f0725676290d99dd7a0d11fe
olesyamaniuk <maniukolesya222@gmail.com>
2024-11-20 15:01:48 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -1,38 +1,4 @@
-$(function () {
-  $('.gallery__list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: false,
-    cssEase: 'linear',
-    prevArrow: '.gallery__list-left',
-    nextArrow: '.gallery__list-right',
-    
-  });
-});
 
-$(function () {
-  $('.special__list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: false,
-    cssEase: 'linear',
-    prevArrow: '.special__list-left',
-    nextArrow: '.special__list-right',
-    
-  });
-});
 
 $(function () {
   $('.features-container').slick({
@@ -54,7 +20,7 @@ $(function () {
 
 
 $(function () {
-  $('.faq__list').slick({
+  $('.popular-container').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -63,13 +29,15 @@ $(function () {
     autoplaySpeed: 3500,
     speed: 1000,
     dots: false,
-    centerMode: true,
+    centerMode: false,
     cssEase: 'linear',
-    prevArrow: '.faq__list-left',
-    nextArrow: '.faq__list-right',
+    prevArrow: '.features-container-left',
+    nextArrow: '.features-container-right',
     
   });
 });
+
+
 
 
 
